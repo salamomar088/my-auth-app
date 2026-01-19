452f5f9f8987294e4ca505e526b004f0677d9057
Omar Salam <salamomar088@gmail.com>
2026-01-19 10:39:43 +0200

    Redesign the sidebar

--- src/app/shared/components/sidebar/sidebar.ts
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.html',
@@ -8,11 +9,14 @@ import { AuthService } from '../../../core/services/auth.service';
   standalone: false,
 })
 export class Sidebar {
+  open = false; // UI ONLY
+
   constructor(private authService: AuthService, private router: Router) {}
 
   logout(): void {
     this.authService.logout();
     this.router.navigate(['/login']);
+    this.open = false;
   }
 
   isLoggedIn(): boolean {
